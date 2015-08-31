bad43de728c712bd5557cc007aa3cd3c5e75103a
Keita Okamoto <ok@aaa.email.ne.jp>
2015-08-31 18:14:29 +0900

    update Gemfile. add harunica

---
+++ packer/scripts/rvm.sh
@@ -0,0 +1,7 @@
+sudo apt-get upgrade -y
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+. /usr/local/rvm/scripts/rvm
+rvm install 2.2.2
+rvm --default use 2.2.2
+sudo apt-get install git -y
